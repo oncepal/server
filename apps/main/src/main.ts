980c4bcc64eab5626dec8bf596cec2fe02089703
Sylas Xu <xymzhs@gmail.com>
2024-09-04 17:52:00 +0800

    chore: 去掉测试微服务

--- apps/main/src/main.ts
+++ apps/main/src/main.ts
@@ -5,17 +5,9 @@ import { InvokeRecordInterceptor } from '@libs/interceptors';
 import helmet from 'helmet';
 import { HttpExceptionFilter } from '@libs/filters';
 import { ValidationPipe } from '@nestjs/common';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
 async function starter() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  const microserviceUser = app.connectMicroservice({
-    transport: Transport.TCP,
-    options: {
-      port: 19961,
-    },
-  });
-
-
  // 全局参数自动过滤
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
@@ -27,8 +19,6 @@ async function starter() {
   app.useGlobalFilters(new HttpExceptionFilter());  
   app.use(helmet());
 
-  
-  await app.startAllMicroservices();
   await app.listen(1996);
 }
 starter();
