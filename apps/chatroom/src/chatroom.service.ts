6e582639410bbe9e5a3e010f0569fd71ab5d6758
Sylas Xu <xymzhs@gmail.com>
2024-09-05 15:26:02 +0800

    feat: 更新聊天相关方法

--- apps/chatroom/src/chatroom.service.ts
+++ apps/chatroom/src/chatroom.service.ts
@@ -4,84 +4,103 @@ import { $Enums } from '@prisma/client';
 
 @Injectable()
 export class ChatroomService {
+  @Inject(PrismaService)
+  private prismaService: PrismaService;
 
-    @Inject(PrismaService)
-    private prismaService: PrismaService;
+  async createSingleChatroom(friendId: number, userId: string) {
+    const { id } = await this.prismaService.chatroom.create({
+      data: {
+        name: '聊天室' + Math.random().toString().slice(2, 8),
+        type: $Enums.ChatroomType.SINGLE,
+      },
+      select: {
+        id: true,
+      },
+    });
 
-    async createOneToOneChatroom(friendId: number, userId: string) {
-        const { id } = await this.prismaService.chatroom.create({
-            data: {
-                name: '聊天室' + Math.random().toString().slice(2, 8),
-                type: $Enums.ChatroomType.SINGLE_CHAT,
-            },
-            select: {
-                id: true
-            }
-        });
+    return '创建成功';
+  }
 
-     
-        return '创建成功'
+  async createGroupChatroom(name: string, userId: string) {
+    const { id } = await this.prismaService.chatroom.create({
+      data: {
+        name,
+        type: $Enums.ChatroomType.SINGLE,
+        users: [userId],
+      },
+    });
+
+    return '创建成功';
+  }
+  async join(id: string, userId: string) {
+    const chatroom = await this.prismaService.chatroom.findUnique({
+      where: {
+        id,
+      },
+    });
+    if (chatroom.type === $Enums.ChatroomType.SINGLE) {
+      throw new BadRequestException('一对一聊天室不能加人');
     }
+    if (chatroom)
+      await this.prismaService.chatroom.update({
+        data: {
+          users: {
+            push: userId,
+          },
+        },
+        where: {
+          id,
+        },
+      });
 
-    async createGroupChatroom(name: string, userId: string) {
-        const { id } = await this.prismaService.chatroom.create({
-            data: {
-                name,
-                type:  $Enums.ChatroomType.MULTIPLE_CHAT,
-                users:[userId],
-            }
-        })
-        
-        return '创建成功'
-    }
-    async join(id: string, userId: string) {
-        const chatroom = await this.prismaService.chatroom.findUnique({
-            where: {
-                id
-            }
-        });
-        if(chatroom.type === $Enums.ChatroomType.SINGLE_CHAT) {
-            throw new BadRequestException('一对一聊天室不能加人');
+    return '加入成功';
+  }
+  async list(userId: string) {
+    const chatroomIds = await this.prismaService.chatroom.findMany({
+      where: {
+        users: {
+          has: userId,
+        },
+      },
+      select: {
+        id: true,
+      },
+    });
+    const chatrooms = await this.prismaService.chatroom.findMany({
+      where: {
+        id: {
+          in: chatroomIds.map((item) => item.id),
+        },
+      },
+      select: {
+        id: true,
+        name: true,
+        type: true,
+      },
+    });
+    return chatrooms;
+  }
+
+
+  async quit(id: string, userId: string) {
+    const chatroom = await this.prismaService.chatroom.findUnique({
+        where: {
+            id
         }
-        if(chatroom)
-            await this.prismaService.chatroom.update({
-                data: {
-                    users: {
-                        push: userId
-                    }
-                },
-                where: {
-                    id
-                }
-            })
-            
+    });
+    if(chatroom.type === $Enums.ChatroomType.SINGLE) {
+        throw new BadRequestException('一对一聊天室不能退出');
+    }
 
-    
-        return '加入成功';
-    }
-    async list(userId: string) {
-        const chatroomIds = await this.prismaService.chatroom.findMany({
-            where: {
-                users:{
-                    has:userId
-                }
-            },
-            select: {
-                id: true
-            }
-        })
-        const chatrooms = await this.prismaService.chatroom.findMany({
-            where: {
-                id: {
-                    in: chatroomIds.map(item => item.id)
-                }
-            },
-            select: {
-                id: true,
-                name: true,
-                type: true,
-            }
-        });
-        return chatrooms;
-    }
+    await this.prismaService.chatroom.update({
+        data: {
+          users: chatroom.users.filter(u=>u!==userId),
+        },
+        where: {
+          id,
+        },
+      });
+
+    return '退出成功';
+}
 }
