fdc10835515c668c7ed5c3adab5e59e15d88fd1d
苑宏博 <9419827+yuan-hongbo@user.noreply.gitee.com>
2024-11-27 09:31:19 +0800

    feat: role guard

--- libs/guards/src/role.guard.ts
+++ libs/guards/src/role.guard.ts
@@ -2,25 +2,26 @@ import { Role } from '@libs/constants';
 import { ROLES_KEY } from '@libs/decorators/roles.decorator';
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-// import { UserService } from 'apps/main/src/user/user.service';
+import { UserService } from 'apps/main/src/user/user.service';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class RoleGuard implements CanActivate {
-  //,private userServie:UserService
-  constructor(private reflector:Reflector){}
+  constructor(private reflector:Reflector,private userServie:UserService){}
   async canActivate(
     context: ExecutionContext,
   ): Promise<boolean> {
 
-    // const requiredRoles = this.reflector.getAllAndMerge<Role[]>(ROLES_KEY,[context.getHandler(),context.getClass()])
-    // if(!requiredRoles){
-    // }
+    const requiredRoles = this.reflector.getAllAndMerge<Role[]>(ROLES_KEY,[context.getHandler(),context.getClass()])
+    if(!requiredRoles){
     return true;
 
-    // const req = context.switchToHttp().getRequest();
-    // const user = await this.userServie.findOneByPhoneNumber(req.user.phoneNumber)
-    // const roleNames = user.roles.map
-    // user
+    }
+
+    const req = context.switchToHttp().getRequest();
+    const user = await this.userServie.findOneById(req.user.userId)
+
+    return requiredRoles.some(o=>o.includes( user.roles));
+
   }
 }
